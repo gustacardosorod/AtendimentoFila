c45711b5fc21cc1856d189b489429ffff8cacd59
gustavocardosorod <gustavocardoso1705@gmail.com>
2024-06-08 11:45:47 -0300

    implementação da lista encadeada

--- src/ListaArrayV2.java
+++ src/ListaArrayV2.java
@@ -1,25 +1,89 @@
-import java.util.ArrayList;
+class Node {
+    Object data;
+    Node next;
+
+    public Node(Object data) {
+        this.data = data;
+        this.next = null;
+    }
+}
 
 public class ListaArrayV2 {
-    private ArrayList<Object> lista;
+    private Node head;
+    private int size;
 
     public ListaArrayV2() {
-        lista = new ArrayList<>();
+        this.head = null;
+        this.size = 0;
     }
 
     public void insere(Object elemento, int posicao) {
-        lista.add(posicao, elemento);
+        if (posicao < 0 || posicao > size) {
+            throw new IndexOutOfBoundsException("Index: " + posicao + ", Size: " + size);
+        }
+
+        Node newNode = new Node(elemento);
+
+        if (posicao == 0) {
+            newNode.next = head;
+            head = newNode;
+        } else {
+            Node current = head;
+            for (int i = 0; i < posicao - 1; i++) {
+                current = current.next;
+            }
+            newNode.next = current.next;
+            current.next = newNode;
+        }
+
+        size++;
     }
 
     public Object seleciona(int posicao) {
-        return lista.get(posicao);
+        if (posicao < 0 || posicao >= size) {
+            throw new IndexOutOfBoundsException("Index: " + posicao + ", Size: " + size);
+        }
+
+        Node current = head;
+        for (int i = 0; i < posicao; i++) {
+            current = current.next;
+        }
+
+        return current.data;
     }
 
     public void remove(int posicao) {
-        lista.remove(posicao);
+        if (posicao < 0 || posicao >= size) {
+            throw new IndexOutOfBoundsException("Index: " + posicao + ", Size: " + size);
+        }
+
+        if (posicao == 0) {
+            head = head.next;
+        } else {
+            Node current = head;
+            for (int i = 0; i < posicao - 1; i++) {
+                current = current.next;
+            }
+            current.next = current.next.next;
+        }
+
+        size--;
     }
 
     public int tamanho() {
-        return lista.size();
+        return size;
+    }
+
+    public static void main(String[] args) {
+        ListaArrayV2 lista = new ListaArrayV2();
+        lista.insere("Elemento 1", 0);
+        lista.insere("Elemento 2", 1);
+        lista.insere("Elemento 3", 2);
+
+        System.out.println("Tamanho: " + lista.tamanho());
+        System.out.println("Elemento na posição 1: " + lista.seleciona(1));
+
+        lista.remove(1);
+        System.out.println("Tamanho após remoção: " + lista.tamanho());
     }
 }
